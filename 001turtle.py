c480c3d36851232e7c4ab9990f3d21b97d788617
18933059@qq.com <18933095@qq.com>
2019-07-02 10:36:37 +0800

    test

---
+++ 001turtle.py
@@ -0,0 +1,12 @@
+import turtle
+
+turtle.pensize(20)
+turtle.pencolor('red')
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(300)
+turtle.mainloop()
